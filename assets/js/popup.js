5fe7b1a19d5fb37e28c90a26e9cc793805af48a6
trolland97 <trolland97@gmail.com>
2023-02-26 21:16:26 +0100

    fixing

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,11 +1,24 @@
-window.addEventListener("load", function () {
+window.addEventListener("load", function(){
     setTimeout(
-        function open(event) {
+        function open(event){
             document.querySelector(".popup").style.display = "block";
+            const body = document.querySelector("body");
+            if (!document.querySelector(".popup").classList.contains("hidden")) {
+                body.style.overflow = "hidden";
+            } else {
+                body.style.overflow = "auto";
+            }
         },
         1000
     )
 });
-document.querySelector("#close").addEventListener("click", function () {
+
+document.querySelector("#close").addEventListener("click", function(){
     document.querySelector(".popup").style.display = "none";
+    document.querySelector("body").style.overflow = "auto";
+});
+
+document.querySelector("#continue").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+    document.querySelector("body").style.overflow = "auto";
 });
